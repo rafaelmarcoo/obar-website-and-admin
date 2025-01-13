eec7af43ad15d83a8d66b03c982fac9478b6f6f2
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-13 17:56:25 +1300

    Added Book button in home

--- src/FrontComponents/HomeSection.jsx
+++ src/FrontComponents/HomeSection.jsx
@@ -13,6 +13,13 @@ const HomeSection = () => {
         <div className="relative z-10 max-w-4xl mx-auto px-4 sm:px-6 text-center">
             <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-white to-red-400">A Korean Feast Awaits</h1>
             <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-medium text-neutral-200 max-w-2xl mx-auto leading-relaxed">Savor the Authentic Korean Flavors of Korea. Experience the Perfect Blend of Tradition and Taste</h2>
+            <div className="flex lg:hidden justify-center items-center text-xl p-10">
+                <a 
+                    className="bg-gradient-to-r from-red-300 to-red-600 p-2 rounded-md"
+                    href="#contact">
+                    Book Now!
+                </a>
+            </div>
         </div>
     </div>
   )
