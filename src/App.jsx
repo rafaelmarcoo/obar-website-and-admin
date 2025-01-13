c4bb535f3dcb4bce60a06b3300f4b9e9fab12cb2
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-13 17:22:10 +1300

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './FrontComponents/Navbar'
 import HomeSection from './FrontComponents/HomeSection'
+import HeroSection from './FrontComponents/HeroSection'
 
 const App = () => {
   return (
     <main className='overflow-y-hidden text-neutral-200 antialiased'>
       <Navbar />
-      <HomeSection />
+      {/* <HomeSection /> */}
+      <HeroSection />
     </main>
   )
 }
