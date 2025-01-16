c06c28caa20d688bbe45d9495f30534d2ef50cfe
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-16 17:24:27 +1300

    Update Footer.jsx

--- src/FrontComponents/Footer.jsx
+++ src/FrontComponents/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer className="w-full py-6 bg-neutral-950 px-6 sm:px-6m x-auto">
         <div className="max-w-7xl mx-auto border-t border-neutral-600">
-            <div className="flex flex-row justify-between mt-6">
-                <h2>
+            <div className="flex flex-col items-center justify-center mt-6">
+                <h2 className="text-center">
                     This website + admin setup is developed by
                     <a 
                         href="https://rafael-manubay.vercel.app/"
@@ -15,7 +15,7 @@ const Footer = () => {
                     </a>
                 </h2>
                 <a
-                    className="hover:text-blue-400 transition-colors duration-200"
+                    className="hover:text-blue-400 transition-colors duration-200 mt-5"
                 >
                     <Home />
                 </a>
